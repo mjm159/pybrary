5430f1400f61bb41f882eac7de4d89074021e37d
Michael Malocha <mmalocha13@gmail.com>
2021-09-29 00:13:43 +0000

    add tests dir & add basic endpoint tests

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,12 +1,20 @@
+# 3RD PARTY MODULES
 import pytest
 
+# LOCAL MODULES
 from app import app
 
+# SETUP & FIXTURES
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+# BASELINE TESTS
 def test_heartbeat(client):
     response = client.get('/heartbeat')
     assert response.status_code == 200
+
+# /api/v1/users* ENDPOINT TESTS
+
+# /api/v1/books* ENDPOINT TESTS
