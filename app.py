9d600c3466bdecea81e053e803ba4b7ed86e1173
Michael Malocha <mmalocha13@gmail.com>
2021-09-29 00:14:30 +0000

    add more flasroutes

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
+# 3RD PARTY MODULES
 from flask import Flask
 
+# SETUP
 app = Flask(__name__)
 
-@app.route("/heartbeat")
+# BASELINE ENDPOINTS
+@app.route("/heartbeat", methods=['GET'])
 def heartbeat():
+    return { "status": "OK" }
+
+# USER ENDPOINTS
+@app.route("/api/v1/users", methods=['GET', 'POST'])
+def users():
+    return { "status": "OK" }
+
+@app.route("/api/v1/users/<email>", methods=['GET', 'PUT'])
+def user(email):
+    return { "status": "OK" }
+
+# WISHLIST ENDPOINTS
+@app.route("/api/v1/users/<email>/wishlist", methods=['GET', 'PUT'])
+def wishlist(email):
+    return { "status": "OK" }
+
+# BOOK ENDPOINTS
+@app.route("/api/v1/books", methods=['GET', 'POST'])
+def books():
+    return { "status": "OK" }
+
+@app.route("/api/v1/books/<book>", methods=['GET', 'PUT'])
+def book(book):
     return { "status": "OK" }
